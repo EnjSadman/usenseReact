34df81521dd1173f2aedd39a066db5d511ce4423
roman <roman.pavlenko.job@gmail.com>
2024-09-20 10:48:52 +0300

    separated find coef

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,9 +1,15 @@
+import { HeaderCurrency } from "./headerCurrency"
+
 const CURRENCIES_LIST = ["USD", "EUR"]
 
 export function Header() {
   return(
     <header>
-      
+      <div className="flex justify-between">
+        {
+          CURRENCIES_LIST.map(el => (<HeaderCurrency currency={el}/>))
+        }
+      </div>
     </header>
   )
 }
--- app/components/headerCurrency.tsx
+++ app/components/headerCurrency.tsx
@@ -1,3 +1,24 @@
-export function HeaderCurrency () {
-  
+"use client"
+
+import { useSelector } from "react-redux"
+import { RootState } from "../store/store"
+import { useMemo } from "react";
+import { findCoef } from "../lib/findCoef";
+import Flag from "react-world-flags";
+
+const FROM_CURRENCY = "UAH";
+
+export function HeaderCurrency ({currency} : {currency : string}) {
+  const { countryCurrency } = useSelector((state : RootState) => state.countryCurrencyReducer);
+
+
+  const from = useMemo(() => (countryCurrency.find(el => el.currencyName === FROM_CURRENCY)), [countryCurrency]) 
+  const to = useMemo(() => (countryCurrency.find(el => el.currencyName === currency)), [countryCurrency]);
+  const coef = useMemo(() => (findCoef(from?.exchangeRate || 1, to?.exchangeRate || 1)), [from, to])
+  return (
+    <div className="flex gap-4">
+      <Flag code={(to?.currencyName === "USD") ? "us" : to?.countryName[0]}/>
+      <p>{coef}</p>
+    </div>
+  )
 }
